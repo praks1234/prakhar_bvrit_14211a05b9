d840dff85cdbda0af1571fd8473d0fd1c10ea6db
praks1234 <31863166+praks1234@users.noreply.github.com>
2017-09-12 14:14:56 +0530

    7th c

--- src/project1/StudentGroup.java
+++ src/project1/StudentGroup.java
@@ -63,6 +63,14 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void addLast(Student student) {
 		// Add your implementation here
+		int n=students.length;
+		Student[] st=new Student[n+1];
+		st[n]=student;
+		for(int i=1;i<n;i++)
+			st[i]=students[i];
+		for(int i=n+1;i<n;i++)
+			
+		students=st;
 	}
 
 	@Override
@@ -74,12 +82,22 @@ public class StudentGroup implements StudentArrayOperation {
 	public void remove(int index) {
 		
 		// Add your implementation here
+		int n=students.length;
+		Student [] ab=new Student[n];
+		for(int i=0,j=0;i<n-1;)
+		{
+			if(i==n)
+				i++;
+			ab[j++]=students[i++];
+				
+		}
+		students=ab;
 		
 	}
 
 	@Override
 	public void remove(Student student) {
-		// Add your implementation here
+		
 	}
 
 	@Override
